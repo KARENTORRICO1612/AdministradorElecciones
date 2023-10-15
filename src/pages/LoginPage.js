5d5efed2e7f2acbc9554aa3955eece803b6b2245
karen <torricokaren12@gmail.com>
2023-10-15 12:15:29 -0400

    Añadiendo condicionales a Login para ingreso y direccionamiento de pantalla

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/normalize.css'
 import '../css/estilos.css'
+import { useNavigate } from 'react-router'
 const LoginPage = () => {
+
+    const [showErrorNombre, setshowErrorName] = useState(false)
+    const [showValorInput, setValorInput] = useState({name:"",password:""})
+
+    const [showContraseña, setContraseña] = useState(false)
+    const [showValorContraseña, setValorContraseña] = useState("")
+
+    let errorNombre = false
+    let errorContraseña = false
+
+
+    const navigate = useNavigate()
+
+    const LoginClick = (e) => {
+        e.preventDefault()
+        if(showValorInput.name.length === 0)
+        {
+            setshowErrorName(true);
+            errorNombre = true
+        }else{
+            setshowErrorName(false)
+            errorNombre = false
+        }
+
+        if(showValorInput.password.length === 0)
+        {
+            setContraseña(true);
+            errorContraseña = true
+        }else{
+            setContraseña(false)
+            errorContraseña = false
+        }
+
+      if(errorNombre === false  && errorContraseña === false )
+      {
+                navigate("/")
+      }
+    }
+
+    const CapturaContenido = (e) => {
+
+        const { name, value } = e.target;
+            setValorInput({
+           ...showValorInput,
+            [name]: value,
+          });
+
+        
+    }
+
+ 
+
   return (
     <div class="body2">
    
@@ -18,11 +71,16 @@ const LoginPage = () => {
             </div>
             <div class="input">
                 <label for="usuario">Usuario</label>
-                <input placeholder="Ingresa tu nombre" type="text" id="usuario"/>
+                <input placeholder="Ingresa tu nombre" type="text" id="usuario"  name="name" onChange={CapturaContenido}/>
+
+                {showErrorNombre && <h4>Por favor ingrese un nombre</h4>}  
+                
             </div>
             <div class="input">
                 <label for="contraseña">Contraseña</label>
-                <input placeholder="Ingresa tu contraseña" type="password" id="contraseña"/>
+                <input placeholder="Ingresa tu contraseña" type="password" name="password" id="contraseña" onChange={CapturaContenido}/>
+
+                {showContraseña && <h4>Por favor ingrese una contraseña</h4>}  
             </div>
 
 
@@ -31,7 +89,7 @@ const LoginPage = () => {
             </div> */}
 
             <br/>
-            <div class="input">
+            <div class="input" onClick={LoginClick}>
                 <input type="submit" value="Ingresar"/>
             </div>
         </form>
