fdbf30dd7788cc45603b4fa8930591ac1c039b10
KaterineRojas <isabelrojascalle.k@gmail.com>
2023-10-16 07:02:34 -0400

    Botones ver eleccion

--- frontend_isa/src/components/VerElecciones.js
+++ frontend_isa/src/components/VerElecciones.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MenuVertical.css';
-const VerElecciones = () => {
+/*const VerElecciones = () => {
   return (
     <table>
       <thead>
@@ -12,7 +12,52 @@ const VerElecciones = () => {
         </tr>
       </thead>
       <tbody>
-        {/* Aquí irán las filas de la tabla */}
+        
+      </tbody>
+    </table>
+  );
+};
+
+export default VerElecciones;*/
+
+
+
+const VerElecciones = () => {
+  const numRows = 4; // Número de filas
+
+  const handleVerDetalleClick = () => {
+    
+    console.log('Ver detalle');
+  };
+
+  // array con el número de filas
+  const rows = Array.from(Array(numRows), (_, index) => (
+    <tr key={index}>
+      <td>Cargo a elección {index + 1}</td>
+      <td>Fecha {index + 1}</td>
+      <td>
+      <button onClick={handleVerDetalleClick} className="ver-detalle-button">
+          Ver Detalle
+        </button>
+      </td>
+      <td>
+        <button onClick={handleVerDetalleClick}className="ver-detalle-button">Ver Convocatoria</button>
+        </td>
+    </tr>
+  ));
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Cargo(s) a elección</th>
+          <th>Fecha</th>
+          <th>Detalle</th>
+          <th>Convocatoria</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows} {}
       </tbody>
     </table>
   );
